c13ba7b8a90ddf7df0ec5dc8fba56b66532b75f0
zhangchen <zhangchen@1670937910092804.onaliyun.com>
2024-08-14 10:38:28 +0800

    init

---
+++ src/main/java/com/example/MainApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DCZhang
+ * @title MainApplication
+ * @date 2024/8/14 10:30
+ * @description TODO
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
